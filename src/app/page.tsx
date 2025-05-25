bb1535d5dbdffd3c032007cefb70a9196f9c878d
Mark L De Guzman <marklester0218@gmail.com>
2025-05-25 20:57:20 +0800

    fix login remove session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   const { data: session } = useSession();
 
   if (!session) {
-    return <Login session={session} />;
+    return <Login />;
   } else {
     return (
       <DashboardLayout>
